3f43a37dc462a00f3fc713b399726e5a0baf7d2e
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2025-02-11 13:36:19 +0000

    Implement exercise 5.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,13 +16,68 @@ const App = () => {
     )  
   }, [])
 
-  return (
+  const handleLogin = async event => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+
+    } catch (error) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">Username:
+            <input
+              id="username"
+              name="username"
+              type="text"
+              value={username}
+              onChange={({ target: { value } }) => setUsername(value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="password">Password:
+            <input
+              id="password"
+              name="password"
+              type="text"
+              value={password}
+              onChange={({ target: { value } }) => setPassword(value)}
+            />
+          </label>
+        </div>
+        <button type="submit">Log In</button>
+      </form>
     </div>
+  )
+
+  return (
+    <>
+      { !user
+        ? loginForm()
+        : (
+          <div>
+            <h2>Blogs</h2>
+            <p>{user.name} logged-in</p>
+            {blogs.map(blog =>
+              <Blog key={blog.id} blog={blog} />
+            )}
+          </div>
+      )}
+    </>
   )
 }
 
