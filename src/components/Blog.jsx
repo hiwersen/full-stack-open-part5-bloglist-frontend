331572f84806fed002c2b0f799bd11b0f7cf1ba8
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2025-02-12 17:05:59 +0000

    Implement exercise 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    Title: {blog.title}{blog.author && `- Author: ${blog.author}`}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [viewDetails, setViewDetails] = useState(false)
+
+  const style = {
+    padding: 12,
+    border: 'solid 1px gray',
+    marginBottom: 8
+  }
+
+  const toggleView = () => (
+    <input 
+    type="button" 
+    value={viewDetails ? "hide" : "view" } 
+    onClick={() => setViewDetails(!viewDetails)} 
+    />
+  )
+
+  const like = () => (
+    <input 
+    type="button" 
+    value="Like" 
+    onClick={() => console.log('Liked!')} 
+    />
+  )
+  
+  return (
+    <div style={style}>
+      <div>{blog.title}&nbsp;{toggleView()}
+      </div>
+      <div style={{ display: viewDetails ? '' : 'none' }}>
+        <div>{blog.url}</div>
+        <div>Likes {blog.likes}&nbsp;{like()}</div>
+        <div>{blog.author}</div>
+      </div>
+    </div>  
+)}
 
 export default Blog
